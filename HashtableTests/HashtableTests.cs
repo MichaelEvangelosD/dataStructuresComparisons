5948acc0f032c52e563354959a6d2413335cf401
MichaelEvangelosD <13-13246@saeinstitute.edu>
2022-10-05 18:14:58 +0300

    Began Hashtable tests

---
+++ HashtableTests/HashtableTests.cs
@@ -0,0 +1,10 @@
+﻿namespace HashtableTests
+{
+    internal class HashtableTests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
